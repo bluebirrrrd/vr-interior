6d342f09f9b3723668d7e6714ee510af06392aa7
Anna Kurylo <anna.kurilo21@gmail.com>
2017-11-08 16:19:02 +0200

    +sphere with basic environment

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview 
+ * @fileoverview
  * This is our main A-Frame application.
  * It defines the main A-Frame Scene which gets mounted root div.
  */
@@ -12,7 +12,10 @@ const COLORS = ['#D92B6A', '#9564F2', '#FFCF59']
 class Main extends Component {
   constructor() {
     super()
-    this.state = { color: 'red' }
+    this.state = {
+      color: 'red',
+      spherePosition: { x: 0.0, y: 4, z: -10.0 }
+    }
   }
 
   changeColor() {
@@ -24,85 +27,42 @@ class Main extends Component {
 
   render() {
     return (
-      <Scene>
-        <a-assets>
-          <img crossOrigin id="groundTexture" src="img/floor.jpg" />
-          <img crossOrigin id="skyTexture" src="img/sky.jpg" />
-        </a-assets>
+      <Scene environment={{
+          preset: 'yavapai',
+          seed: 2,
+          lightPosition: { x: 0.0, y: 0.03, z: -0.5 },
+          fog: 0.8,
+          ground: 'hills',
+          groundYScale: 6.31,
+          groundTexture: 'none',
+          groundColor: '#553e35',
+          grid: 'none'
+        }}>
 
-        <Entity
-          primitive="a-plane"
-          src="#groundTexture"
-          rotation="-90 0 0"
-          height="100"
-          width="100"
-        />
-        <Entity primitive="a-light" type="ambient" color="#445451" />
-        <Entity
-          primitive="a-light"
-          type="point"
-          intensity="2"
-          position="2 4 4"
-        />
-        <Entity
-          primitive="a-sky"
-          height="2048"
-          radius="30"
-          src="#skyTexture"
-          theta-length="90"
-          width="2048"
-        />
-        <Entity particle-system={{ preset: 'snow', particleCount: 2000 }} />
-        <Entity
-          text={{ value: 'Hello, A-Frame Preact!', align: 'center' }}
-          position={{ x: 0, y: 2, z: -1 }}
-        />
-
-        <Entity
-          id="box"
-          geometry={{ primitive: 'box' }}
-          material={{ color: this.state.color, opacity: 0.6 }}
-          animation__rotate={{
-            property: 'rotation',
-            dur: 2000,
-            loop: true,
-            to: '360 360 360'
-          }}
-          animation__scale={{
-            property: 'scale',
-            dir: 'alternate',
-            dur: 100,
-            loop: true,
-            to: '1.1 1.1 1.1'
-          }}
-          position={{ x: 0, y: 1, z: -3 }}
-          events={{ click: this.changeColor.bind(this) }}
-        >
-          <Entity
-            animation__scale={{
-              property: 'scale',
-              dir: 'alternate',
-              dur: 100,
-              loop: true,
-              to: '2 2 2'
-            }}
-            geometry={{ primitive: 'box', depth: 0.2, height: 0.2, width: 0.2 }}
-            material={{ color: '#24CAFF' }}
-          />
-        </Entity>
-
-        <Entity primitive="a-camera">
+        <Entity primitive="a-camera" look-controls>
           <Entity
             primitive="a-cursor"
-            animation__click={{
-              property: 'scale',
-              startEvents: 'click',
-              from: '0.1 0.1 0.1',
-              to: '1 1 1',
-              dur: 150
-            }}
+            cursor={{ fuse: false }}
+            material={{ color: 'white', shader: 'flat', opacity: 0.75 }}
+            geometry={{ radiusInner: 0.005, radiusOuter: 0.007 }}
           />
         </Entity>
+
+        <Entity
+          primitive="a-octahedron"
+          detail={2}
+          radius={2}
+          position={ this.state.spherePosition }
+          color="#FAFAF1"
+        />
+
+        <Entity
+          primitive="a-light"
+          type="directional"
+          color="#FFF"
+          intensity={1}
+          position={{ x: 2.5, y: 0.0, z: 0.0 }}
+        />
       </Scene>
     )
   }
